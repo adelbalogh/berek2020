0a06571eaec49f4d16151f11188306c2f9bf0589
balogh.adel <balogh.adel@28-DIAK5>
2024-11-18 15:03:30 +0100

    Add project files.

---
+++ Berek.cs
@@ -0,0 +1,24 @@
+﻿namespace berek2020
+{
+    internal class Berek
+    {
+        public string nev { get; set; }
+        public string neme { get; set; }
+        public string reszleg { get; set; }
+        public int belepes { get; set; }
+        public int ber { get; set; }
+
+        public override string ToString() =>   
+            $"\n\tNév: {nev}\n\tNem: {neme}\n\tBelépés: {belepes}\n\tBér: {ber}";
+
+        public Berek(string sor)
+        {
+            string[] b = sor.Split(';');
+            nev = b[0];
+            neme = b[1];
+            reszleg = b[2];
+            belepes = int.Parse(b[3]);
+            ber = int.Parse(b[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using berek2020;
+using System.Text;
+
+List<Berek> berek = [];
+
+using StreamReader sr = new("..\\..\\..\\src\\berek2020.txt", Encoding.UTF8);
+while (!sr.EndOfStream) berek.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"3. Feladat: Dolgozók száma: {berek.Count}");
+
+var f4 = berek.Average(e => e.ber/1000);
+Console.WriteLine($"4. Feladat: Bérek átlaga: {f4:0.0} eFt");
+
+Console.Write("5. Feladat: Kérem a részleg nevét: ");
+string ker=Console.ReadLine();
+var f5 = berek.Where(b => b.reszleg==ker);
+var f6 = berek.Where(b => b.reszleg==ker).MaxBy(b => b.ber);
+
+Console.WriteLine($"6. Feladat: {f6}");
+
+
+var f7 = berek.GroupBy(e => e.reszleg).OrderBy(g => g.Key);
+Console.WriteLine("7. Feladat: Statisztika");
+foreach (var grp in f7)
+{
+    Console.WriteLine($"\t{grp.Key,8} - {grp.Count(),2} fő");
+}
